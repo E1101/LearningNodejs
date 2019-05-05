7ebe35e029bd1825f43f510d37e8f2a52fa44d8f
Payam <naderi.payam@gmail.com>
2019-05-05 19:29:35 +0300

    arguments; throw error

--- task.js
+++ task.js
@@ -1,3 +1,5 @@
+const util = require('util');
+
 function add(a, b)
 {
   return a + b;
@@ -5,13 +7,27 @@ function add(a, b)
 
 function sum()
 {
+  // arguments is an Array-like object accessible inside functions
+  // that contains the values of the arguments passed to that function.
+  // exp. {'0': 10, '1': 5, '2': 6}
+
+  // here we transform that object into array, the result is:
+  // [10, 5, 6]
+  // because as i mentioned the arguments is Array-like it means that it
+  // dosen't contains .forEach() or map()
   let numbers = Array.from(arguments);
 
   let sum = 0;
-  numbers.forEach(num => sum += num);
+  numbers.forEach(num => {
+    if ('number' != typeof num)
+      throw Error(util.format('Argument Should Be number; given (%s)%s.', typeof num, num));
+
+    sum += num
+  });
+
   return sum;
 }
 
 // testing
-console.log(add(10, 6));    // 16
-console.log(sum(10, 5, 6)); // 21
+console.log( add(10, 6) );    // 16
+console.log( sum(10, 4.5, 6) );     // 20.5
